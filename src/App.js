518cc2f4a5e2b2a5a73dd1c7e3721f3f4a43deba
aelsonsantos <aelsonsantos121@gmail.com>
2024-07-30 16:22:18 -0300

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { IoMdSearch } from "react-icons/io";
+import api from './services/api';
+import './style.css';
+
 
 function App() {
+
+  const [Input,setInput] = useState('');
+  const [cep, setCep] =useState({})
+
+
+  async function searchCep(){
+    if (Input === ''){
+      alert("Por favor, digite o CEP!");
+      return;
+    }
+
+    try {
+      const response = await api.get(`${Input}/json`);
+      setCep(response.data);
+      setInput("");
+    } catch{
+      alert("Erro ao buscar o CEP, tente novamente!");
+      setInput("");
+    }
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Buscador de CEP</h1>
+
+      <div className="inputContainer">
+        <input 
+          type="text"
+          placeholder="Informe CEP desejado"
+          value={Input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+
+        <button className="search" onClick={searchCep}>
+          <IoMdSearch size={25} color="#fff"/>
+        </button>
+      </div>
+        
+        {Object.keys(cep).length > 0 && ( 
+          <main className="mainSearch">
+            <h2>{cep.cep}</h2>
+
+            <span>{cep.logradouro}</span>
+            <span>{cep.complemento}</span>
+            <span>{cep.bairro}</span>
+            <span>{cep.localidade} - {cep.uf}</span>
+          </main>
+        )}
+        
     </div>
   );
 }
